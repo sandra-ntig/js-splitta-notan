0ef6f4a790befcd816d860af209b1d8425ffe796
sandra-ntig <88836988+sandra-ntig@users.noreply.github.com>
2021-12-17 11:16:17 +0100

    changed files

---
+++ js/index.js
@@ -0,0 +1,32 @@
+//Dessa är alla variabler du behöver använda. 
+
+let sum;
+let numberOfFriends;
+let tip;
+let calculatedTip;
+let total;
+let sumDivided;
+let friendSum;
+
+
+// Detta ska hända när man klickar på knappen
+document.getElementById('calculateButton').addEventListener('click', function() {
+
+    //Hämtar det som skrivits i inputfältet med ID sum, numberOfFriends, tip
+  
+
+    // Omvandla variabler (default text, d.v. string) till integer eller float. Använd parseInt() eller parseFloat()
+ 
+  
+    
+    //Räkna ut vad varje vän ska betala
+  
+
+    //Skriv ut värdet i h3 med id result
+  
+
+    // Dölj formuläret med id inputForm och visa section med id showSum. Använd classList()
+ 
+
+
+})
